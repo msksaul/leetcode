7418dda575622f217725568519c53c64568e06a2
Melvin Calel <msksaul@gmail.com>
2025-09-10 10:42:03 -0600

    Time: 2 ms (85.61%) | Memory: 60.2 MB (14.65%) - LeetSync

---
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.ts
@@ -0,0 +1,17 @@
+function minSubArrayLen(target: number, nums: number[]): number {
+  let min = Infinity
+  let sum = 0
+  let L = 0
+
+  for(let R=0; R<nums.length; R++) {
+    sum+=nums[R]
+
+    while(sum>=target) {
+      min = Math.min(min, R-L+1)
+      sum-=nums[L]
+      L++
+    }
+  }
+
+  return min==Infinity ? 0 : min
+};
