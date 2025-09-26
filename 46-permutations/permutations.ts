a3f5d843cd34914eb385d69879c5c37f26f8cbf1
Melvin Calel <msksaul@gmail.com>
2025-09-26 13:11:06 -0600

    Time: 4 ms (33.68%) | Memory: 60.9 MB (33.16%) - LeetSync

---
+++ 46-permutations/permutations.ts
@@ -0,0 +1,21 @@
+function permute(nums: number[]): number[][] {
+  let ans = []
+  let perm = []
+
+  const backtrack = () => {
+    if(perm.length==nums.length) {
+      ans.push([...perm])
+    }
+
+    for(const x of nums) {
+      if(!perm.includes(x)) {
+        perm.push(x)
+        backtrack()
+        perm.pop()
+      }
+    }
+  }
+
+  backtrack()
+  return ans
+};
