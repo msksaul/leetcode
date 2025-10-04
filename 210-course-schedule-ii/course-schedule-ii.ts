b77dd2399d8704bc23c4dbf0fa535b1ba9e0cacb
Melvin Calel <msksaul@gmail.com>
2025-10-04 11:59:41 -0600

    Time: 16 ms (34.13%) | Memory: 63.8 MB (35.47%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.ts
@@ -0,0 +1,40 @@
+function findOrder(numCourses: number, prerequisites: number[][]): number[] {
+  let order = []
+  let graph = new Map()
+
+  for(const [a,b] of prerequisites) {
+    if(!graph.has(a)) {
+      graph.set(a, [])
+    }
+    graph.get(a).push(b)
+  }
+
+  const unvisited = 0
+  const visiting = 1
+  const visited = 2
+
+  let states = new Array(numCourses).fill(unvisited)
+
+  const dfs = (node) => {
+    if(states[node]==visited) return true
+    if(states[node]==visiting) return false
+
+    states[node] = visiting
+
+    if(graph.has(node)) {
+      for(const neighbor of graph.get(node)) {
+        if(!dfs(neighbor)) return false
+      }
+    }
+
+    states[node] = visited
+    order.push(node)
+    return true
+  }
+
+  for(let i=0; i<numCourses; i++) {
+    if(!dfs(i)) return []
+  }
+
+  return order
+};
