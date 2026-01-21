bc296849bd939ff2051272fa1e52229447da6f28
Melvin Calel <msksaul@gmail.com>
2026-01-21 13:09:55 -0600

    Time: 5 ms (94.70%) | Memory: 56.7 MB (90.48%) - LeetSync

--- 242-valid-anagram/valid-anagram.ts
+++ 242-valid-anagram/valid-anagram.ts
@@ -1,4 +1,13 @@
 function isAnagram(s: string, t: string): boolean {
   if(s.length !== t.length) return false
-  return s.split('').sort().join('')==t.split('').sort().join('')
+  
+  let sk = new Array(26).fill(0)
+  let tk = new Array(26).fill(0)
+  
+  for(let i=0; i<s.length; i++) {
+    sk[s[i].charCodeAt(0)-97]++
+    tk[t[i].charCodeAt(0)-97]++
+  }
+  
+  return sk.join('')===tk.join('')
 };
