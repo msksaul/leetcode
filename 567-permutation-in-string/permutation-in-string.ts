f3993901887d52e9fa4c45356e84b454f9bce081
Melvin Calel <msksaul@gmail.com>
2026-03-10 12:10:02 -0600

    Time: 193 ms (12.73%) | Memory: 63.3 MB (25.46%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.ts
+++ 567-permutation-in-string/permutation-in-string.ts
@@ -1,24 +1,18 @@
 function checkInclusion(s1: string, s2: string): boolean {
-  const n1 = s1.length
-  const n2 = s2.length
-
-  if(n1>n2) return false
-
-  let c1 = new Array(26).fill(0)
-  let c2 = new Array(26).fill(0)
-
-  for(let i=0; i<n1; i++) {
-    c1[s1[i].charCodeAt(0)-97]++
-    c2[s2[i].charCodeAt(0)-97]++
+  if(s1.length>s2.length) return false
+  let fs1 = new Array(26).fill(0)
+  
+  for(const char of s1) {
+    fs1[char.charCodeAt(0)-97]++
   }
+  const fq1 = fs1.join('#')
 
-  if(c1.toString()==c2.toString()) return true
-
-  for(let i=n1; i<n2; i++) {
-    c2[s2[i].charCodeAt(0)-97]++
-    c2[s2[i-n1].charCodeAt(0)-97]--
-    
-    if(c1.toString()==c2.toString()) return true
+  for(let i=0; i<=s2.length-s1.length; i++) {
+    let fs2 = new Array(26).fill(0)
+    for(let j=i; j<i+s1.length; j++) {
+      fs2[s2[j].charCodeAt(0)-97]++
+    }
+    if(fq1 == fs2.join('#')) return true
   }
 
   return false
