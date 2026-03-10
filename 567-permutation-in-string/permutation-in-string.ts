eef0e1b2d75df94eba77431cadf38238ddada2da
Melvin Calel <msksaul@gmail.com>
2026-03-10 12:15:05 -0600

    Time: 220 ms (12.73%) | Memory: 63.7 MB (20.33%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.ts
+++ 567-permutation-in-string/permutation-in-string.ts
@@ -5,14 +5,13 @@ function checkInclusion(s1: string, s2: string): boolean {
   for(const char of s1) {
     fs1[char.charCodeAt(0)-97]++
   }
-  const fq1 = fs1.join('#')
 
   for(let i=0; i<=s2.length-s1.length; i++) {
     let fs2 = new Array(26).fill(0)
     for(let j=i; j<i+s1.length; j++) {
       fs2[s2[j].charCodeAt(0)-97]++
     }
-    if(fq1 == fs2.join('#')) return true
+    if(fs1.toString() == fs2.toString()) return true
   }
 
   return false
